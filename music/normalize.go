fc890914a883cebcf5434095c51c5911aff41af9
Maxime Gosselin <maxime.gosselin@cssspnql.com>
2024-01-10 22:22:49 -0500

    fix: missing normalized notes

--- music/normalize.go
+++ music/normalize.go
@@ -2,22 +2,24 @@ package music
 
 func normalizeName(n string) string {
 	switch n {
-	case "B#":
-		return "C"
+	case "Cb":
+		return "B"
 	case "Db":
 		return "C#"
 	case "Eb":
 		return "D#"
-	case "Fb":
-		return "E"
 	case "E#":
 		return "F"
+	case "Fb":
+		return "E"
 	case "Gb":
 		return "F#"
 	case "Ab":
 		return "G#"
 	case "Bb":
 		return "A#"
+	case "B#":
+		return "C"
 	default:
 		return n
 	}
